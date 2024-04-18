631e171bc927289f2bf70f77c4ff0cc209332126
AmanPunia-510 <puniaaman888@gmail.com>
2024-04-18 11:01:18 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(".slider").slick({
+  arrows: false,
+  dots: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  variableWidth: true,
+  slidesToShow: 6,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 2000,
+  pauseOnHover: true,
+  cssEase: "linear",
+});
+
+$(".slick-slider2").slick({
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  infinite: true,
+  centerMode: true,
+});
