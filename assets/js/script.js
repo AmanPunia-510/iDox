bae8fc9b39544f52d7bc9c9f4cbd38b39fb03cb7
AmanPunia-510 <puniaaman888@gmail.com>
2024-04-19 12:46:08 +0530

    work on code alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,27 @@ $(".slick-slider2").slick({
   centerMode: true,
   dots: true,
 });
+
+const preloader = document.querySelector(".preloader");
+
+setTimeout(() => {
+  preloader.classList.add("d-none");
+}, 2000);
+
+const item = document.querySelector(".navbar-items");
+const menu = document.querySelector(".toggle");
+
+menu.addEventListener("click", () => {
+  item.classList.toggle("show");
+    let contain = document.querySelector(".all-content");
+  if (item.classList.contains("show")) {
+    contain.style.position = "fixed";
+    contain.style.top = "0";
+    contain.style.left = "0";
+    contain.style.width = "100%";
+  }
+
+  else {
+    contain.style.position = "unset";
+  }
+});
