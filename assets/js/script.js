f3842e9e960da51c13daf7f59c62b6f52209a597
AmanPunia-510 <puniaaman888@gmail.com>
2024-04-19 12:48:56 +0530

    some change in css alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,15 +32,13 @@ const menu = document.querySelector(".toggle");
 
 menu.addEventListener("click", () => {
   item.classList.toggle("show");
-    let contain = document.querySelector(".all-content");
+  let contain = document.querySelector(".all-content");
   if (item.classList.contains("show")) {
     contain.style.position = "fixed";
     contain.style.top = "0";
     contain.style.left = "0";
     contain.style.width = "100%";
-  }
-
-  else {
+  } else {
     contain.style.position = "unset";
   }
 });
